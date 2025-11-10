642719378afacc141290346bfe5a1e1920eedc8d
tamara <tamarazivkovic9200@gmail.com>
2025-11-10 08:22:54 +0100

    back-end

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 app.use(logger('dev'));
 
 // Add the petRouter to the `/pets` route
+
 app.use('/pets', petRouter);
-app.use(cors({ origin: 'http://localhost:5173' }));
+
 
 
 app.listen(PORT, () => {
